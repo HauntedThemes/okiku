bff93a61a2e8b57be0be200dce06373659525a77
Nistor Cristian <contact@nistorcristian.com>
2019-01-29 17:51:41 +0200

    Improved load on home.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,9 @@
 jQuery(document).ready(function($) {
 
     var config = {
-        'share-selected-text': true,
-        'load-more': true,
-        'infinite-scroll': false,
-        'infinite-scroll-step': 1,
-        'disqus-shortname': 'hauntedthemes-demo'
+        'disqus-shortname': 'hauntedthemes-demo',
+        'content-api-host': 'http://localhost:2368',
+        'content-api-key': '8a13e02a8917186f02014db742',
     };
 
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
@@ -18,6 +16,12 @@ jQuery(document).ready(function($) {
         lang = $('html').attr('lang'),
         noBookmarksMessage = $('.no-bookmarks').html(),
         monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    var ghostAPI = new GhostContentAPI({
+        host: config['content-api-host'],
+        key: config['content-api-key'],
+        version: 'v2'
+    });
 
     var currentPage = 1;
     var pathname = window.location.pathname;
@@ -139,9 +143,152 @@ jQuery(document).ready(function($) {
         });
     }
 
-    var rellax = new Rellax('.rellax', {
-        center: true,
-    });
+    if($('.rellax').length){
+        var rellax = new Rellax('.rellax', {
+            center: true,
+        });
+    }
+
+    // Check 'read later' posts 
+    if (typeof Cookies.get('okiku-read-later') !== "undefined") {
+        readLaterPosts = JSON.parse(Cookies.get('okiku-read-later'));
+    }
+
+    readLaterPosts = readLater($('#content .loop'), readLaterPosts);
+
+    function readLater(content, readLaterPosts) {
+
+        if (typeof Cookies.get('okiku-read-later') !== "undefined") {
+            $.each(readLaterPosts, function(index, val) {
+                $('.read-later[data-id="' + val + '"]').addClass('active');
+            });
+            bookmarks(readLaterPosts);
+        }
+
+        $(content).find('.read-later').each(function(index, el) {
+            $(this).on('click', function(event) {
+                event.preventDefault();
+                var id = $(this).attr('data-id');
+                if ($(this).hasClass('active')) {
+                    removeValue(readLaterPosts, id);
+                } else {
+                    readLaterPosts.push(id);
+                };
+                $('.read-later[data-id="' + id + '"]').each(function(index, el) {
+                    $(this).toggleClass('active');
+                });
+                $('header .counter').addClass('shake');
+                setTimeout(function() {
+                    $('header .counter').removeClass('shake');
+                }, 300);
+                Cookies.set('okiku-read-later', readLaterPosts, {
+                    expires: 365
+                });
+                bookmarks(readLaterPosts);
+            });
+        });
+
+        return readLaterPosts;
+
+    }
+
+    function bookmarks(readLaterPosts) {
+
+        $('.bookmark-container').empty();
+        if (readLaterPosts.length) {
+
+            var url = [location.protocol, '//', location.host].join('');
+
+            $('header .counter').removeClass('hidden').text(readLaterPosts.length);
+            $('.bookmark-container').removeClass('no-bookmarks');
+
+            var filter = readLaterPosts.toString();
+            filter = "id:[" + filter + "]";
+
+            ghostAPI.posts
+                .browse({limit: 'all', filter: filter, include: 'tags'})
+                .then((results) => {
+
+                    $('.bookmark-container').empty();
+
+                    $.each(results, function(index, result) {
+                        var dateSplit = result.published_at.split('T');
+                        dateSplit = dateSplit[0].split('-');
+                        var month = monthNames.indexOf(dateSplit[1])+1;
+                        var date = moment(dateSplit[2]+'-'+dateSplit[1]+'-'+dateSplit[0], "DD-MM-YYYY").format('DD MMM YYYY');
+                        var tag = '';
+                        if(result.primary_tag){
+                            tag = '<span class="tags"><a href="/tag/'+ result.primary_tag.slug +'">'+ result.primary_tag.name +'</a></span>';
+                        }
+                        var str = '\
+                        <div class="item"> \
+                         <article> \
+                           <div class="post-inner-content"> \
+                               <p> \
+                                 <a href="/' + result.slug + '/" class="post-title" title="' + result.title + '"><strong>' + result.title + '</strong></a> \
+                               </p> \
+                           </div> \
+                           <div class="post-meta"> \
+                               <time datetime="' + result.published_at + '">' + date + '</time>' + tag + ' \
+                               <div class="inner"> \
+                                 <a href="https://twitter.com/share?text=' + encodeURIComponent(result.title) + '&amp;url=' + url + result.slug + '" class="twitter" onclick="window.open(this.href, \'share-twitter\', \'width=550,height=235\');return false;"><i class="fab fa-twitter"></i></a> \
+                                 <a href="#" class="read-later active" data-id="' + result.id + '"><i class="far fa-bookmark"></i></a> \
+                               </div> \
+                           </div> \
+                         </article> \
+                        </div>';
+                        $('.bookmark-container').append(str);
+                    });
+
+                    $('.bookmark-container').find('.read-later').each(function(index, el) {
+                        $(this).on('click', function(event) {
+                            event.preventDefault();
+                            var id = $(this).attr('data-id');
+                            if ($(this).hasClass('active')) {
+                                removeValue(readLaterPosts, id);
+                            }else{
+                                readLaterPosts.push(id);
+                            };
+                            $('.read-later[data-id="'+ id +'"]').each(function(index, el) {
+                                $(this).toggleClass('active');
+                            });
+                            Cookies.set('poveglia-read-later', readLaterPosts, { expires: 365 });
+                            bookmarks(readLaterPosts);
+                        });
+                    });
+
+                    if (results) {
+                        $('header .counter').removeClass('hidden').text(results.length);
+                    }else{
+                        $('header .counter').addClass('hidden');
+                        $('.bookmark-container').append('<p class="no-bookmarks"></p>');
+                        $('.no-bookmarks').html(noBookmarksMessage)
+                    };
+
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+
+        } else {
+            $('header .counter').addClass('hidden');
+            $('.bookmark-container').append('<p class="no-bookmarks"></p>');
+            $('.no-bookmarks').html(noBookmarksMessage)
+        };
+    }
+
+    function removeValue(arr) {
+        var what, a = arguments,
+            L = a.length,
+            ax;
+        while (L > 1 && arr.length) {
+            what = a[--L];
+            while ((ax = arr.indexOf(what)) !== -1) {
+                arr.splice(ax, 1);
+            }
+        }
+        return arr;
+    }
 
     // $('#search-field').on('focusin', function () {
     //     $('.search form').addClass('active');
